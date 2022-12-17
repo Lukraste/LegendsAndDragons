2978868e0ad80d16dcd6acfba2097190c47479e0
olivier1990 <olivierstordiaugithub@gmail.com>
2022-12-17 11:54:56 +0100

    -Supprimer login.cs et placer => Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿//using Jeu_de_role_POO;
+using GameRpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace GameRpg
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            //**
+            //MENU
+            //*
+            Console.Title = "Legends and Dragons";
+            // Text
+            MainMenu.Menu();
+            MainMenu.WriteLogo();
+            Console.WriteLine("Bienvenue");
+            Console.ReadKey(true);
+
+            //**
+            //MAPPING
+            //**
+            //Console.WriteLine("Legends and dragons");
+            //Map map = new();
+            //map.AfficheMap();
+
+        }
+    }
+}
+
